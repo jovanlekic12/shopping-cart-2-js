83affd6007e3e0ec3c07f7ef5a3ad8ac38f2be77
jovanlekic12 <lekicjovan1212@gmail.com>
2024-06-30 18:19:12 +0200

    add data

---
+++ data.js
@@ -0,0 +1,26 @@
+export const item = [
+  {
+    name: "Protein powder",
+    price: 85,
+    quantity: 1,
+    maxQuantity: 5,
+  },
+  {
+    name: "Creatine Monohydrate",
+    price: 30,
+    quantity: 1,
+    maxQuantity: 10,
+  },
+  {
+    name: "Glutamine",
+    price: 40,
+    quantity: 1,
+    maxQuantity: 12,
+  },
+  {
+    name: "BCAA",
+    price: 35,
+    quantity: 1,
+    maxQuantity: 15,
+  },
+];
